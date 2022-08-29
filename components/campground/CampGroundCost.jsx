e9d9f199be47b4ea82e200e60df0bb4dd8d40299
grohiit <81778041+grohiit@users.noreply.github.com>
2022-08-29 11:26:48 -0400

    name change

--- components/campground/CampGroundCost.jsx
+++ components/campground/CampGroundCost.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CampGroundCost = ({ campgroundCost, name }) => {
   return (
     <>
--- components/campground/CampGroundRules.jsx
+++ components/campground/CampGroundRules.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CampGroundRules = ({ campgroundRules, name }) => {
   return (
     <>
