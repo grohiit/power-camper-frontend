28540b2384595f57cbb3dd013eb184665206d83c
grohiit <81778041+grohiit@users.noreply.github.com>
2022-10-24 17:40:38 -0400

    added right favicon finally

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -34,7 +34,7 @@ export default function Layout({
           key="product-jsonld"
         />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
@@ -78,7 +78,7 @@ export default function Layout({
         <meta
           property="twitter:image"
           content="https://power-camper2.vercel.app/seoSecond.webp"
-        />
+        /> */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -96,7 +96,6 @@ export default function Layout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" /> */}
       </Head>
       <Navbar />
       <main>{children}</main>
