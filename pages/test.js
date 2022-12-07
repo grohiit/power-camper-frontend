8b9bc91f2eb9ac97edbb669f9c15ae2b254352a9
grohiit <81778041+grohiit@users.noreply.github.com>
2022-12-07 17:58:53 -0500

    fixed ordered list showing as unordered list

--- pages/test.js
+++ pages/test.js
@@ -1,28 +1,20 @@
-import DetailSections from '@/components/campground/DetailSections'
-import { API_CAMPGROUND_URL } from '@/config/index'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import Layout from '@/components/Layout'
 
 export default function TestPage({ pagedata }) {
-  const campgroundpageData = pagedata && pagedata.data ? pagedata.data : {}
-
   return (
-    <>
-      <DetailSections campgroundpageData={campgroundpageData} />
-    </>
+    <Layout>
+      <h1>Test Page</h1>
+      <ol>
+        <li>
+          Urgent - I need a campsite <strong>today</strong>!
+        </li>
+        <li>
+          I need a campsite <strong>within the next week or two</strong>
+        </li>
+        <li>
+          I need a campsite <strong>within the next few months</strong>
+        </li>
+      </ol>
+    </Layout>
   )
-}
-
-export async function getServerSideProps({ params }) {
-  //   API CALL FOR PAGE DATA
-  const slug = 'el-capitan-state-beach'
-  const PAGE_URL = `${API_CAMPGROUND_URL}/${slug}`
-  const res = await fetch(PAGE_URL)
-  const pagedata = await res.json()
-
-  return {
-    props: {
-      pagedata,
-    },
-  }
 }
